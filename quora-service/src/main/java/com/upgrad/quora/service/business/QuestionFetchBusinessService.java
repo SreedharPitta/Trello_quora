30671dbd8fe7bd3aea3df363adf0d02abf497c70
Sreedhar <sreedhar@Sreedhars-MacBook-Pro.local>
2021-03-29 10:52:05 +0530

    Changes for Get Question Details Service

---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionFetchBusinessService.java
@@ -0,0 +1,51 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.QuestionDAO;
+import com.upgrad.quora.service.dao.UserDAO;
+import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class QuestionFetchBusinessService {
+
+    @Autowired
+    private QuestionDAO questionDAO;
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Autowired
+    private UserAuthenticationBusinessService userAuthenticationBusinessService;
+
+    //To get All Questions Posted by All Users
+    public List<QuestionEntity> getAllQuestions(final String authorizationToken) throws AuthorizationFailedException {
+        UserAuthEntity userAuthEntity = userAuthenticationBusinessService.authenticateUser(authorizationToken);
+        if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to get all questions");
+        }
+        return questionDAO.getAllQuestions();
+    }
+
+    //To get All Questions Posted by a Single User
+    public List<QuestionEntity> getUserAllQuestions(String userId, String authorization) throws AuthorizationFailedException, UserNotFoundException {
+        UserAuthEntity userAuthEntity = userAuthenticationBusinessService.authenticateUser(authorization);
+        if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to get all questions posted by a specific user");
+        }
+        UserEntity userEntity = userDAO.getUser(userId);
+        if (userEntity != null) {
+            return questionDAO.getUserAllQuestions(userEntity);
+        } else {
+            throw new UserNotFoundException("USR-001", "User with entered uuid whose question details are to be seen does not exist");
+        }
+    }
+}
