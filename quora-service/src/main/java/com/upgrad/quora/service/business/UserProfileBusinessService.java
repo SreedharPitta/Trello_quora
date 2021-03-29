38368491ad980f0de34f3d9cdf53424c02ebbb36
Sreedhar <sreedhar@Sreedhars-MacBook-Pro.local>
2021-03-29 11:01:26 +0530

    Added Transactional Notation for User Profile Service

--- quora-service/src/main/java/com/upgrad/quora/service/business/UserProfileBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserProfileBusinessService.java
@@ -7,6 +7,8 @@ import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserProfileBusinessService {
@@ -14,6 +16,7 @@ public class UserProfileBusinessService {
     @Autowired
     private UserDAO userDAO;
 
+    @Transactional(propagation = Propagation.REQUIRED)
     public UserEntity getUser(final String userId, final String accessToken) throws UserNotFoundException, AuthorizationFailedException {
         UserAuthEntity userAuthEntity = userDAO.getUserAuthToken(accessToken);
         if (userAuthEntity == null) {
